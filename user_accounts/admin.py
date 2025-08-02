4d77d98d77219a8f503df197e8e6b56588c77e35
ismoil <ismoilumarzoda@gmail.com>
2025-08-02 12:55:37 +0500

    fix admin2 com

--- user_accounts/admin.py
+++ user_accounts/admin.py
@@ -1,12 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-class CustomUserAdmin(UserAdmin):
-    list_display = ('email', 'fullname', 'is_staff')
-    
-    list_filter = ('is_staff', 'is_superuser', 'is_active' )
-    
-    search_fields = ('email', 'fullname','phone_number')
 
-admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(CustomUser)
