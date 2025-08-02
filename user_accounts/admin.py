d218cb49fcf0974ceca6ce643cd84bc4d15ab4b2
ismoil <ismoilumarzoda@gmail.com>
2025-08-02 12:51:47 +0500

    fix admin com

--- user_accounts/admin.py
+++ user_accounts/admin.py
@@ -3,10 +3,10 @@ from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    list_display = ('email', 'fullname', 'is_staff')
     
-    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    list_filter = ('is_staff', 'is_superuser', 'is_active' )
     
-    search_fields = ('username', 'email', 'first_name', 'last_name')
+    search_fields = ('email', 'fullname','phone_number')
 
 admin.site.register(CustomUser, CustomUserAdmin)
